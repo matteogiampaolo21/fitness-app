c78c93511eacc9517c9df402b203a2ac4b7a9d64
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2024-05-13 15:03:10 +0700

    adding and deleting works with firebase

--- src/app/intake/@records/[exercise]/page.tsx
+++ src/app/intake/@records/[exercise]/page.tsx
@@ -1,17 +1,44 @@
 'use client'
-import React, { useState } from 'react'
-import { records} from '../page'
+import React, { useEffect, useState } from 'react'
+import { Records} from '../page'
+import { getDoc, doc, Timestamp, updateDoc } from 'firebase/firestore'
+import { db } from '../../../../../firebase/firebaseConfig'
 import { useParams, useRouter } from 'next/navigation'
+
+
 
 const Page = () => {
   const params = useParams<{exercise:string}>();
   const router = useRouter();
+  
+  const [weight,setWeight] = useState(0);
+  const [records, setRecords] = useState<Records>({});
+  const [isLoading, setLoading] = useState(true)
+  
+  async function getData(){
+    const docRef = doc(db, "records", "ZJgsvvE5ArG3KWH1JxUX");
+    const docSnap = await getDoc(docRef);
+  
+  
+    if (docSnap.exists()) {
+      console.log(docSnap.data()[params.exercise][0].date);
+      console.log(docSnap.data()[params.exercise]);
+      // const now = new Date()
+      // const date = Timestamp.fromDate(now);
+      // console.log(date.toDate())
+      setRecords( docSnap.data()  );
+      setLoading(false)
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  }
 
-  const [weight,setWeight] = useState(0)
+  useEffect(() => {
+    getData();
+  },[])
 
-  console.log(typeof params.exercise);
-
-  function handleSubmit(e:React.MouseEvent<HTMLButtonElement, MouseEvent>){
+  async function handleSubmit(e:React.MouseEvent<HTMLButtonElement, MouseEvent>){
     e.preventDefault();
 
     if(weight === 0 ){
@@ -19,16 +46,43 @@ const Page = () => {
       return
     }
 
-    records[params.exercise].push({weight:weight,date:new Date()})
+    const tempArray = records[params.exercise];
+    tempArray.push({weight:weight,date: Timestamp.fromDate(new Date())})
+
+
+    const recordRef = doc(db, "records", "ZJgsvvE5ArG3KWH1JxUX");
+
+    // Set the "capital" field of the city 'DC'
+    await updateDoc(recordRef, {
+      [params.exercise]: tempArray,
+    });
+
+    // records[params.exercise].push({weight:weight,date: Timestamp.fromDate(new Date())})
     setWeight(0)
   }
 
-  function deleteEntry(timeOfEntry:number){
-    console.log("entry deleted");
-    console.log(records[params.exercise])
-    const updatedLogs = records[params.exercise].filter((entry) => entry.date.getTime() !== timeOfEntry)
-    console.log(updatedLogs)
+  async function deleteEntry(timeOfEntry:number){
+    const updatedLogs = records[params.exercise].filter((entry) => entry.date.toDate().getTime() !== timeOfEntry)
+    
+    
+    const recordRef = doc(db, "records", "ZJgsvvE5ArG3KWH1JxUX");
+    
+    // Set the "capital" field of the city 'DC'
+    await updateDoc(recordRef, {
+      [params.exercise]: updatedLogs,
+    });
 
+    const tempArray = records[params.exercise].filter(entry =>
+      entry.date.toDate().getTime() !== timeOfEntry
+    )
+    setRecords(prev => {
+      return {
+        ...prev,
+        [params.exercise]: tempArray,
+      }
+    })
+    console.log("entry deleted");
+    // console.log(records)
   }
 
 
@@ -50,16 +104,24 @@ const Page = () => {
         <h2 className='text-lg font-bold'>{params.exercise} records</h2>
 
         <ul>
-          {records[params.exercise].map((entry) => {
-            return (
-              <li  className='flex flex-row gap-5 mb-3 group duration-200'  key={entry.date.getTime()}>
-                <p>
-                  {entry.weight}KG | {entry.date.getTime()}
-                </p>
-                <button onClick={() => deleteEntry(entry.date.getTime())} className={`bg-neutral-300 rounded invisible group-hover:visible px-2 hover:bg-red-500 hover:text-white duration-200`}>Delete</button>
-              </li>
-            )
-          })}
+
+          {isLoading ?
+            <div>loading</div>
+            :
+            <div>
+              {records[params.exercise].map((entry:{weight:number,date:Timestamp}) => {
+                return (
+                  <li  className='flex flex-row gap-5 mb-3 group duration-200'  key={entry.date.toDate()}>
+                    <p>
+                      {entry.weight}KG | {`${entry.date.toDate()}`}
+                    </p>
+                    <button onClick={() => deleteEntry(entry.date.toDate().getTime())} className={`bg-neutral-300 rounded invisible group-hover:visible px-2 hover:bg-red-500 hover:text-white duration-200`}>Delete</button>
+                  </li>
+                )
+              })}
+            </div>
+          
+          }
         </ul>
       </article>
     </div>
--- src/app/intake/@records/page.tsx
+++ src/app/intake/@records/page.tsx
@@ -1,26 +1,48 @@
 import Link from 'next/link'
 import React from 'react'
+import {doc, getDoc, Timestamp} from 'firebase/firestore'
+import {db} from '../../../../firebase/firebaseConfig'
 
-export let records:Records = {
-  backSquats:[
-    {weight:80,date:new Date('October 23, 2021')},
-    {weight:83,date:new Date('November 04, 2021')},
-    {weight:97,date:new Date('November 05,2021')},
-  ],
-  pullUps:[
-    {weight:15,date:new Date('January 15, 2022')},
-    {weight:24,date:new Date('April 18, 2022')}
-  ]
-}
+
+// export let records:Records = {
+//   backSquats:[
+//     {weight:80,date:new Date('October 23, 2021')},
+//     {weight:83,date:new Date('November 04, 2021')},
+//     {weight:97,date:new Date('November 05,2021')},
+//   ],
+//   pullUps:[
+//     {weight:15,date:new Date('January 15, 2022')},
+//     {weight:24,date:new Date('April 18, 2022')}
+//   ]
+// }
 
 export type Records = {
   [key:string]:{
    weight:number,
-   date:Date, 
+   date:Timestamp, 
   }[]
 }
 
-function page() {
+async function page() {
+
+
+  async function getData(){
+    const docRef = doc(db, "records", "ZJgsvvE5ArG3KWH1JxUX");
+    const docSnap = await getDoc(docRef);
+
+  
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      return docSnap.data();
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  }
+  
+  const records = await getData();
+
+
   return (
     <article className='border-black border rounded p-3'>
       <h2 className='text-lg'>Personal Records</h2>
